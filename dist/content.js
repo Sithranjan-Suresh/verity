77d9f264f6281a445446355d1a5d7929a0dd58f1
Sithranjan-Suresh <119703037+Sithranjan-Suresh@users.noreply.github.com>
2026-03-01 18:39:54 -0600

    Delete dist directory

--- dist/content.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click", (e) => {
-    const target = e.target;
-    const link = target.closest("a");
-    if (link && link.href) {
-        console.log("Clicked URL:", link.href);
-    }
-});
