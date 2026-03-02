e1d2a06075a27df7b91999cf244eec2e19313e0d
Sithranjan-Suresh <119703037+Sithranjan-Suresh@users.noreply.github.com>
2026-03-01 18:49:54 -0600

    Delete src directory

--- src/content.ts
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", (e) => {
-  const target = e.target as HTMLElement;
-  const link = target.closest("a");
-
-  if (link && link.href) {
-    console.log("Clicked URL:", link.href);
-  }
-});
